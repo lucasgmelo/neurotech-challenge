d4a5608c57d1e9bf6a3a331d06c099929f9c7307
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-17 21:33:35 -0300

    feat(main): create login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Input } from '../styles/global';
+// import { LoginContainer } from '../styles/loginStyles';
+
+export default function Login() {
+  const history = useHistory();
+
+  return (
+    <>
+      <>
+        <strong>do it!</strong>
+        <img src="/images/desktop_logo.svg" alt="logo" />
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            history.push('/dashboard');
+          }}
+        >
+          <label htmlFor="email">Email</label>
+          <Input required id="email" type="text" />
+          <label htmlFor="password">Senha</label>
+          <Input required id="password" type="password" />
+          <Button>Continuar</Button>
+        </form>
+      </>
+    </>
+  );
+}
