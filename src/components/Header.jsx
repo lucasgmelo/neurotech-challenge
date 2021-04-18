31cc92195721b385def7237c0bf503a6f27bf769
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-18 00:03:50 -0300

    feat(main): create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { useHistory } from "react-router-dom";
+// import { MyHeader } from "../styles/dashboardStyles";
+
+export function Header() {
+  const history = useHistory();
+
+  return (
+    <>
+      <div>
+        <button type="button" onClick={() => history.push("/")}>
+          <img src="/images/exit.svg" alt="Sair" />
+          <p>Sair</p>
+        </button>
+      </div>
+      <img src="/images/small_logo.svg" alt="Logo" />
+    </>
+  );
+}
